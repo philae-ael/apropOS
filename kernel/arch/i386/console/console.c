fbce5191d7cbc6dcfebfa2913928c9f80897e0fd
philae-ael <1511446+philae-ael@users.noreply.github>
2017-01-07 22:43:34 +0100

    fix scroll bug on console

--- kernel/arch/i386/console/console.c
+++ kernel/arch/i386/console/console.c
@@ -37,7 +37,7 @@ void scrolls(uint8_t lineToScrolls){
     uint16_t* start_clear = get_address(0, console_status.screen.height - lineToScrolls);
     uint16_t* end = get_address(console_status.screen.width, console_status.screen.height);
 
-    memcpy(start, console, (end - start) * sizeof(uint16_t));
+    memmove(console, start, (end - start) * sizeof(uint16_t));
 
     memset(start_clear, 0, (end - start_clear) * sizeof(uint16_t));
 }
@@ -53,6 +53,15 @@ void console_init(){
 }
 
 void console_putc(char c){
+
+    if(console_status.cursor.x > console_status.screen.width){
+        console_status.cursor.x = 0;
+        console_status.cursor.y += 1;
+    }
+    if(console_status.cursor.y >= console_status.screen.height){
+        scrolls(1);
+        console_status.cursor.y--;
+    }
     uint16_t* address = get_address(console_status.cursor.x,
                                   console_status.cursor.y);
     switch(c){
@@ -66,11 +75,9 @@ void console_putc(char c){
     case '\b':
         if(console_status.cursor.x > 0)
             console_status.cursor.x -= 1;
-        else{
-            if(console_status.cursor.x > 0)
-                console_status.cursor.y -= 1;
-            console_status.cursor.x = console_status.screen.width;
-        }
+
+        address = get_address(console_status.cursor.x,
+                                  console_status.cursor.y);
         *address = (0x0F << 8) | 0;
         break;
 
@@ -78,14 +85,6 @@ void console_putc(char c){
         *address = (0x0F << 8) | c;
 
         console_status.cursor.x += 1;
-    }
-
-    if(console_status.cursor.x > console_status.screen.width){
-        console_status.cursor.x = 0;
-        if(console_status.cursor.y == console_status.screen.height)
-            scrolls(1);
-        else
-            console_status.cursor.y += 1;
     }
 
 }
